ee974637e81ac5e8cc16735e91e4ab923e2d36ee
mac <mac@sitvmac.local>
2018-08-01 14:49:42 +0800

    pic

---
+++ Picture/TableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  TableViewCell.swift
+//  newshopapp
+//
+//  Created by mac on 2018/8/1.
+//  Copyright © 2018年 mac. All rights reserved.
+//
+
+import UIKit
+
+class TableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
